ef2e128112a49312d2f7eecb755223c079360303
Formerscholar <2319315514@qq.com>
2020-11-07 18:35:26 +0800

    11/7 可选categoryId where

--- middleware/article.js
+++ middleware/article.js
@@ -3,10 +3,12 @@ const { paging } = require('../utils')
 
 module.exports = {
   async getlist(req, res, next) {
-    let { page = 1 } = req.query
+    let { page = 1, CategoryId  } = req.query
     ~~page < 1 ? 1 : ~~page
+    let where = ~~CategoryId ? { CategoryId } : {}
     try {
       const data = await db.Article.findAll({
+        where,
         include: [
           {
             model: db.Category,
@@ -23,8 +25,8 @@ module.exports = {
               {
                 model: db.User,
                 attributes: ['id', 'nickname', 'avatar', 'gender'],
-              }
-            ]
+              },
+            ],
           },
         ],
         order: [['updatedAt', 'DESC']],
